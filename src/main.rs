1c7a41e33acca09924d8428005024ec0daf0d53e
Ng Guoyou <fauxface+github@gmail.com>
2017-10-11 06:00:03 +0800

    Unimplement i16/u16, exit process on completion
    Also remove MidIter

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,14 @@ extern crate futures;
 extern crate synthrs;
 
 use std::thread;
-use std::sync::{Arc, Mutex};
-use std::ops::Deref;
+use std::sync::Arc;
 
 use futures::stream::Stream;
 use futures::task;
 use futures::task::Executor;
 use futures::task::Run;
 
-use synthrs::synthesizer::{SamplesIter, quantize, make_samples_from_midi};
-use synthrs::wave::SineWave;
+use synthrs::synthesizer::make_samples_from_midi;
 
 struct MyExecutor;
 
@@ -21,21 +19,6 @@ impl Executor for MyExecutor {
         r.run();
     }
 }
-
-struct MidIter {
-    samples: Vec<f64>,
-    i: usize
-}
-
-impl Iterator for MidIter {
-    type Item = f64;
-
-    fn next(&mut self) -> Option<f64> {
-        self.i = self.i + 1;
-        self.samples.get(self.i - 1).cloned()
-    }
-}
-
 
 fn main() {
     let endpoint = cpal::get_default_endpoint().unwrap();
@@ -49,51 +32,36 @@ fn main() {
 
     let (mut voice, stream) = cpal::Voice::new(&endpoint, &format, &event_loop).unwrap();
 
-    // This works
-    // let mut data_source = SamplesIter::new(format.samples_rate.0 as u64, Box::new(SineWave(440.0)));
-
-    let samples = make_samples_from_midi(format.samples_rate.0 as usize, "mountainking.mid");
-    let mut data_source = MidIter { samples: samples, i: 0 };
-
-    // This doesn't
-    // let mut data_source = samples.iter().cloned();
-    // let mut data_source = Arc::new(make_samples_from_midi(44100, "mountainking.mid").iter().cloned());
+    let source_samples = make_samples_from_midi(format.samples_rate.0 as usize, "mountainking.mid");
+    let mut data_source = Arc::new(source_samples.into_iter());
 
     task::spawn(stream.for_each(move |buffer| -> Result<_, ()> {
-        // let mut data_source = Arc::get_mut(&mut data_source).unwrap();
+        let mut data_source = Arc::get_mut(&mut data_source).unwrap();
+        let channels = format.channels.len();
+
         match buffer {
-            cpal::UnknownTypeBuffer::U16(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(
-                    &mut data_source,
-                )
-                {
-                    for out in sample.iter_mut() {
-                        *out = 0 // quantize::<u16>(value); // might be really loud
-                    }
-                }
+            cpal::UnknownTypeBuffer::U16(mut _buffer) => {
+                unimplemented!();
             }
-            cpal::UnknownTypeBuffer::I16(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(
-                    &mut data_source,
-                )
-                {
-                    for out in sample.iter_mut() {
-                        *out = 0 // quantize::<i16>(value); // might be really loud
-                    }
-                }
+            cpal::UnknownTypeBuffer::I16(mut _buffer) => {
+                // `quantize` can be used to convert the f64 to i16/u16, but
+                // since it cannot be tested on my machines it is left unimplemented
+                // quantize::<i16>(value); // might be really loud
+                unimplemented!();
             }
             cpal::UnknownTypeBuffer::F32(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(
-                    &mut data_source,
-                )
-                {
+                let zipped = buffer.chunks_mut(channels).zip(&mut data_source);
+                if zipped.size_hint() == (0, Some(0)) {
+                    ::std::process::exit(0);
+                }
+
+                for (sample, value) in zipped {
                     for out in sample.iter_mut() {
                         *out = value as f32;
                     }
                 }
             }
         }
-
         Ok(())
     })).execute(executor);
 
