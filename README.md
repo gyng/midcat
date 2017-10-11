3d2e3a12715929757078ce5a65c1986df839185a
Ng Guoyou <ng.guoyou@gmail.com>
2017-10-11 12:57:37 +0800

    Add CLI parsing

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # midcat
 
-WIP. Example repo for synthrs-cpal integration.
+Example application for using [synthrs](https://github.com/gyng/synthrs) with [cpal](https://github.com/tomaka/cpal).
+
+Plays a MIDI file (ignoring instruments) from the command line.
+
+## Usage
+
+```
+$ cargo +nightly run -- foobar.mid
+$ cargo +nightly run -- --help
+```
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
 extern crate cpal;
 extern crate futures;
 extern crate synthrs;
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
 
 use std::thread;
 use std::sync::Arc;
 
+use docopt::Docopt;
 use futures::stream::Stream;
 use futures::task;
 use futures::task::Executor;
 use futures::task::Run;
 
 use synthrs::synthesizer::make_samples_from_midi;
+// use synthrs::wave::SineWave;
+// use synthrs::synthesizer::SamplesIter;
+
+const USAGE: &'static str = "
+Play a MIDI file, ignoring instruments
+
+Usage:
+  midcat <file> [--volume=<frac>]
+  midcat (-h | --help)
+  midcat --version
+
+Options:
+  -h --help     Show this screen
+  -v=<frac> --volume=<frac>   Play volume as a fraction (linear scale) [default: 1.0]
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_file: String,
+    flag_volume: f32,
+}
 
 struct MyExecutor;
 
@@ -21,22 +46,30 @@ impl Executor for MyExecutor {
 }
 
 fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
     let endpoint = cpal::get_default_endpoint().unwrap();
     let format = endpoint
         .get_supported_formats_list()
         .unwrap()
         .next()
         .unwrap();
+
     let event_loop = cpal::EventLoop::new();
     let executor = Arc::new(MyExecutor);
+    let (mut voice, stream) =
+        cpal::Voice::new(&endpoint, &format, &event_loop).expect("failed to create voice/stream");
 
-    let (mut voice, stream) = cpal::Voice::new(&endpoint, &format, &event_loop).unwrap();
-
-    let source_samples = make_samples_from_midi(format.samples_rate.0 as usize, "mountainking.mid");
+    let source_samples =
+        make_samples_from_midi(format.samples_rate.0 as usize, &args.arg_file);
     let mut data_source = Arc::new(source_samples.into_iter());
 
+    // let mut data_source = SamplesIter::new(format.samples_rate.0 as u64, Box::new(SineWave(440.0)));
+
     task::spawn(stream.for_each(move |buffer| -> Result<_, ()> {
-        let mut data_source = Arc::get_mut(&mut data_source).unwrap();
+        let mut data_source = Arc::get_mut(&mut data_source).expect("failed to get arc");
         let channels = format.channels.len();
 
         match buffer {
@@ -57,7 +90,7 @@ fn main() {
 
                 for (sample, value) in zipped {
                     for out in sample.iter_mut() {
-                        *out = value as f32;
+                        *out = args.flag_volume * value as f32;
                     }
                 }
             }
@@ -65,9 +98,7 @@ fn main() {
         Ok(())
     })).execute(executor);
 
-    thread::spawn(move || {
-        voice.play();
-    });
+    thread::spawn(move || { voice.play(); });
 
     event_loop.run();
 }
